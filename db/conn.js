22cf0ed4e3d96a10c4f491d8b20833a6458a6c08
leo-robson <leo.robson@outlook.com>
2022-10-12 17:45:16 -0300

    Arquivo Final

---
+++ db/conn.js
@@ -0,0 +1,14 @@
+const {Sequelize} = require('sequelize')
+
+const sequelize = new Sequelize('toughts2','root','',{
+    host:'localhost',
+    dialect:'mysql'
+})
+try{
+    sequelize.authenticate()
+    console.log('Conectado com Sucesso')
+}catch(err){
+    console.log(`Não foi possivel conectar ${err}`)
+}
+
+module.exports = sequelize
